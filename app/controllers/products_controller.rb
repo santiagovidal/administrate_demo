d29c1476cac8429c457de74391c9983a7675bc8d
santiagovidal <santiago.vidal@toptierlabs.com>
2016-10-14 15:22:42 -0300

    Completing controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,34 @@
 class ProductsController < ApplicationController
+  before_action :get_product, only: [:show, :destroy]
+
   def index
     @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params.merge(user_id: current_user.id))
+    redirect_to @product
+  end
+
+  def show
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description)
+  end
+
+  def get_product
+    @product = Product.find(params[:id])
   end
 end
